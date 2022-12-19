7b75d21f063cd9487f47378d595890cf1b28b7a4
Liza McLain <lizamclain@gmail.com>
2022-12-18 23:08:19 -0600

    commit tree 925d4d7f38a55212c6419a7588c5291a025167ea

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sam"]
+
+function findMatching(drivers, string){
+    let matches = drivers.filter(person => person === string);
+    return matches
+    // if (typeof person === 'string') {
+    //     return matches.names
+    // }
+}
+
+function fuzzyMatch(drivers, string){
+    let fuzzyMatch = drivers.filter(person => person[0].indexOf(string) === 0);
+    return fuzzyMatch
+}
+
+function matchName (drivers, string){
+    let lastMatch = drivers.filter(person => person === string);
+    return lastMatch.hometown
+}
